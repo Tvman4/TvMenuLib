fe8cd82ebc93dc8a25bb2bf0a6277616347f2dd8
Tvman <elijahpauley702@gmail.com>
2026-08-16 15:43:08 -0400

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := TvMenu
-LOCAL_SRC_FILES := main.cpp Menu.cpp
+LOCAL_SRC_FILES := main.cpp Menu.cpp Mods.cpp
 LOCAL_LDLIBS    := -llog -landroid -lGLESv2
 LOCAL_CPPFLAGS  := -std=c++11 -fvisibility=hidden
 
