b7b73553e50d4c4451d3350395886c8e1808f07f
Tvman <elijahpauley702@gmail.com>
2026-08-16 15:49:05 -0400

    Update Menu.h

--- jni/Menu.h
+++ jni/Menu.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <jni.h>
+#include <string>
 
 class Menu {
 public:
     static bool isOpen;
+    static bool notificationsEnabled;
     static void InitMenu();
     static void RenderUI();
     static bool CheckYButtonInput();
     static void UpdateHandPosition();
+    static void ShowNotification(std::string message);
 };
