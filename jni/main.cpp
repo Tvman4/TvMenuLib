959745494a79b4f184669a841124b2829962b04c
Tvman <elijahpauley702@gmail.com>
2026-08-18 19:41:00 -0400

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -4,15 +4,15 @@
 #include <pthread.h>
 #include "Menu.h"
 
-#define TAG "GtagCopyMain"
+#define TAG "TvMenuQuestMain"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 
 void* engine_loop(void*) {
-    LOGI("Gtag Copy Engine Loop Active across Unity 2019-2022 LTS.");
-    Menu::InitMenu();
+    LOGI("TvMenuQuest Engine Loop Active across Unity 2019-2022 LTS.");
+    TvMenuQuest::InitMenu();
     
     while (true) {
-        Menu::RenderUI();
+        TvMenuQuest::RenderUI();
         usleep(50000); // 20 FPS heartbeat loop
     }
     return nullptr;
