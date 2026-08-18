bb987051b1b5164a7494deebe905ef32d3f56e49
Tvman <elijahpauley702@gmail.com>
2026-08-18 19:32:40 -0400

    Update Menu.h

--- jni/Menu.h
+++ jni/Menu.h
@@ -1,15 +1,26 @@
 #pragma once
 #include <jni.h>
 #include <string>
+#include <vector>
+
+struct Category {
+    std::string name;
+    std::vector<std::string> mods;
+};
 
 class Menu {
 public:
     static bool isOpen;
     static bool notificationsEnabled;
+    static int currentCategoryIndex;
+    static int selectedModIndex;
+    static const std::vector<Category> categories;
+
     static void InitMenu();
     static void RenderUI();
-    static bool CheckYButtonInput();
-    static void UpdateHandPosition();
-    static void CheckPointerInteraction(); // Added declaration
+    static void ScrollLeft();
+    static void ScrollRight();
+    static void SelectCurrentMod();
     static void ShowNotification(std::string message);
+    static void DisconnectLobbyGlobal();
 };
