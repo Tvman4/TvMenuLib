c6102a5815badd33d9200cb2a5bdaaad5462f887
Tvman <elijahpauley702@gmail.com>
2026-08-20 16:47:32 -0400

    Update Mods.h

--- jni/Mods.h
+++ jni/Mods.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 
 struct Vector3 {
     float x, y, z;
@@ -7,9 +8,12 @@ struct Vector3 {
 
 class Mods {
 public:
-    static void ExecuteUniversalMod(std::string modName, bool state);
+    static void ExecuteUniversalMod(const std::string& modName, bool state);
     static void SetTimeScale(float scale);
     static void SetGravity(float x, float y, float z);
-    static void ApplyTransformScaleQuery(std::string targetNode, float scaleMultiplier);
+    static void ApplyTransformScale(const std::string& targetNode, float scaleMultiplier);
     static void DisconnectNetwork();
+    static void SetFly(bool enabled);
+    static void SetNoclip(bool enabled);
+    static void SetGodMode(bool enabled);
 };
