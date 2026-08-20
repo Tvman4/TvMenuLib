633bf0b1fb2c58318344f45649691fe72c9a87c9
Tvman <elijahpauley702@gmail.com>
2026-08-20 19:47:22 -0400

    Update Mods.h

--- jni/Mods.h
+++ jni/Mods.h
@@ -1,29 +1,18 @@
 #pragma once
 #include <string>
 
-struct Vector3 {
-    float x, y, z;
-};
-
 class Mods {
 public:
     static void ExecuteUniversalMod(const std::string& modName, bool state);
 
-    static void SetTimeScale(float scale);
-    static void SetGravity(float x, float y, float z);
-
-    static void ApplyTransformScale(const std::string& targetNode, float scaleMultiplier);
     static void SetLongArms(bool enabled);
-    static void SetPlayerScale(float scale);
-    static void SetArmScale(float scale);
-
-    static void TagAll(bool enabled);
-    static void OrbitPlayers(bool enabled);
-    static void Crasher(bool enabled);
-    static void UnlockAllCosmetics(bool enabled);
-
+    static void SetSpeed(bool enabled);
     static void SetFly(bool enabled);
     static void SetNoclip(bool enabled);
-    static void SetGodMode(bool enabled);
-    static void DisconnectNetwork();
+    static void SetGravity(bool enabled);
+    static void TagAll(bool enabled);
+    static void Orbit(bool enabled);
+    static void Crasher(bool enabled);
+    static void UnlockCosmetics(bool enabled);
+    static void GodMode(bool enabled);
 };
