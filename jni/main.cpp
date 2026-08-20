c6a16d16cfbc32eedddb8f1aab34f1d77bc35bbc
Tvman <elijahpauley702@gmail.com>
2026-08-20 19:54:11 -0400

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -14,36 +14,26 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "TvMenuMain", __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "TvMenuMain", __VA_ARGS__)
 
-typedef EGLBoolean (*eglSwapBuffers_t)(EGLDisplay dpy, EGLSurface surface);
+typedef EGLBoolean (*eglSwapBuffers_t)(EGLDisplay, EGLSurface);
 static eglSwapBuffers_t original_eglSwapBuffers = nullptr;
-
 static bool imgui_initialized = false;
 
 void InitImGui() {
     if (imgui_initialized) return;
-
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-
     ImGui::StyleColorsDark();
     ImGui_ImplOpenGL3_Init("#version 300 es");
-
     imgui_initialized = true;
-    LOGI("ImGui initialized");
+    LOGI("ImGui ready");
 }
 
 EGLBoolean hooked_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
-    if (!imgui_initialized) {
-        InitImGui();
-    }
+    if (!imgui_initialized) InitImGui();
 
     ImGui_ImplOpenGL3_NewFrame();
     ImGui::NewFrame();
-
     TvMenuQuest::Draw();
-
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
@@ -52,37 +42,18 @@ EGLBoolean hooked_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
 
 bool InstallHook() {
     void* handle = dlopen("libEGL.so", RTLD_LAZY);
-    if (!handle) {
-        LOGE("Failed to open libEGL.so");
-        return false;
-    }
-
+    if (!handle) return false;
     void* addr = dlsym(handle, "eglSwapBuffers");
-    if (!addr) {
-        LOGE("eglSwapBuffers not found");
-        return false;
-    }
+    if (!addr) return false;
 
-    if (DobbyHook(addr, (void*)hooked_eglSwapBuffers, (void**)&original_eglSwapBuffers) == 0) {
-        LOGI("eglSwapBuffers hooked successfully!");
-        return true;
-    }
-
-    LOGE("DobbyHook failed");
-    return false;
+    return DobbyHook(addr, (void*)hooked_eglSwapBuffers, (void**)&original_eglSwapBuffers) == 0;
 }
 
 void* MainThread(void*) {
     sleep(5);
-
     TvMenuQuest::Init();
-
-    if (InstallHook()) {
-        LOGI("Hook installed - menu should appear");
-    } else {
-        LOGE("Failed to install hook");
-    }
-
+    if (InstallHook()) LOGI("Hook success - menu should appear");
+    else LOGE("Hook failed");
     return nullptr;
 }
 
