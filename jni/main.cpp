dda10af0cc3fc7d75088cd7e5c7d8d2e9353f306
Tvman <elijahpauley702@gmail.com>
2026-08-20 19:48:11 -0400

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,31 +1,94 @@
 #include <jni.h>
 #include <android/log.h>
+#include <dlfcn.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+#include "dobby.h"
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
 #include "Menu.h"
 
-#define TAG "TvMenuQuestMain"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "TvMenuMain", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "TvMenuMain", __VA_ARGS__)
 
-void* engine_loop(void*) {
-    // Wait for Unity to fully load (important to prevent crash)
-    sleep(10);
+typedef EGLBoolean (*eglSwapBuffers_t)(EGLDisplay dpy, EGLSurface surface);
+static eglSwapBuffers_t original_eglSwapBuffers = nullptr;
 
-    LOGI("TvMenuQuest starting after delay...");
-    TvMenuQuest::InitMenu();
+static bool imgui_initialized = false;
 
-    while (true) {
-        TvMenuQuest::UpdateInput();
-        TvMenuQuest::RenderUI();
-        usleep(20000);
+void InitImGui() {
+    if (imgui_initialized) return;
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+
+    ImGui::StyleColorsDark();
+    ImGui_ImplOpenGL3_Init("#version 300 es");
+
+    imgui_initialized = true;
+    LOGI("ImGui initialized");
+}
+
+EGLBoolean hooked_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    if (!imgui_initialized) {
+        InitImGui();
     }
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+
+    TvMenuQuest::Draw();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    return original_eglSwapBuffers(dpy, surface);
+}
+
+bool InstallHook() {
+    void* handle = dlopen("libEGL.so", RTLD_LAZY);
+    if (!handle) {
+        LOGE("Failed to open libEGL.so");
+        return false;
+    }
+
+    void* addr = dlsym(handle, "eglSwapBuffers");
+    if (!addr) {
+        LOGE("eglSwapBuffers not found");
+        return false;
+    }
+
+    if (DobbyHook(addr, (void*)hooked_eglSwapBuffers, (void**)&original_eglSwapBuffers) == 0) {
+        LOGI("eglSwapBuffers hooked successfully!");
+        return true;
+    }
+
+    LOGE("DobbyHook failed");
+    return false;
+}
+
+void* MainThread(void*) {
+    sleep(5);
+
+    TvMenuQuest::Init();
+
+    if (InstallHook()) {
+        LOGI("Hook installed - menu should appear");
+    } else {
+        LOGE("Failed to install hook");
+    }
+
     return nullptr;
 }
 
 extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-    pthread_t pt;
-    pthread_create(&pt, nullptr, engine_loop, nullptr);
-    pthread_detach(pt);
-    LOGI("TvMenuQuest .so injected (delayed start)");
+    pthread_t t;
+    pthread_create(&t, nullptr, MainThread, nullptr);
+    pthread_detach(t);
     return JNI_VERSION_1_6;
 }
