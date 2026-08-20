84810b3a41075b7b76d8d7f3ea0badf2444e42c6
Tvman <elijahpauley702@gmail.com>
2026-08-20 17:29:38 -0400

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -8,7 +8,7 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 
 void* engine_loop(void*) {
-    LOGI("TvMenuQuest started – Long Arms world-scale mode active");
+    LOGI("TvMenuQuest Engine Loop Active – Unity 2021.3 LTS");
     TvMenuQuest::InitMenu();
 
     while (true) {
@@ -23,6 +23,6 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     pthread_t pt;
     pthread_create(&pt, nullptr, engine_loop, nullptr);
     pthread_detach(pt);
-    LOGI("TvMenuQuest .so injected");
+    LOGI("Library injected – Unity 2021.3 mode");
     return JNI_VERSION_1_6;
 }
