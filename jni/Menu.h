6cf47a9059c14ec3bac2fe8aea0e2a80fa4d4253
Tvman <elijahpauley702@gmail.com>
2026-08-20 19:46:29 -0400

    Update Menu.h

--- jni/Menu.h
+++ jni/Menu.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <string>
 #include <vector>
-#include <map>
-#include <mutex>
 
 struct Category {
     std::string name;
@@ -12,31 +10,12 @@ struct Category {
 class TvMenuQuest {
 public:
     static bool isOpen;
-    static bool notificationsEnabled;
-    static int currentCategoryIndex;
-    static int selectedModIndex;
+    static int currentCategory;
+    static int currentMod;
 
-    static const std::string menuTitle;
     static const std::vector<Category> categories;
 
-    static void InitMenu();
-    static void RenderUI();
-    static void UpdateInput();
-    static void ScrollLeft();
-    static void ScrollRight();
-    static void ScrollUp();
-    static void ScrollDown();
-    static void SelectCurrentMod();
-    static void ShowNotification(const std::string& message);
-    static void DisconnectLobbyGlobal();
-    static void SaveConfig();
-    static void LoadConfig();
-    static bool IsModActive(const std::string& modName);
-
-private:
-    static std::map<std::string, bool> activeModStates;
-    static std::mutex stateMutex;
-
-    static bool lastA, lastB, lastX, lastY;
-    static bool lastLeft, lastRight, lastUp, lastDown;
+    static void Init();
+    static void Draw();
+    static void Toggle();
 };
