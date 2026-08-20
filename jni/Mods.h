15cc030a96a231d85a88ff812345cb58f7072361
Tvman <elijahpauley702@gmail.com>
2026-08-20 17:08:47 -0400

    Update Mods.h

--- jni/Mods.h
+++ jni/Mods.h
@@ -9,18 +9,24 @@ class Mods {
 public:
     static void ExecuteUniversalMod(const std::string& modName, bool state);
 
-    // Core working systems
+    // Core
     static void SetTimeScale(float scale);
     static void SetGravity(float x, float y, float z);
+
+    // Scale systems
     static void ApplyTransformScale(const std::string& targetNode, float scaleMultiplier);
+    static void SetLongArms(bool enabled);          // world-under-you style
     static void SetPlayerScale(float scale);
     static void SetArmScale(float scale);
 
-    // Movement
+    // Requested features
+    static void TagAll(bool enabled);
+    static void OrbitPlayers(bool enabled);
+    static void Crasher(bool enabled);
+    static void UnlockAllCosmetics(bool enabled);
+
     static void SetFly(bool enabled);
     static void SetNoclip(bool enabled);
     static void SetGodMode(bool enabled);
-
-    // Network
     static void DisconnectNetwork();
 };
