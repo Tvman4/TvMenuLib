d9f03f8109f4059fd554d75daa4bd42c2ea8b5f4
Tvman <elijahpauley702@gmail.com>
2026-08-20 19:46:10 -0400

    Update Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := arm64-v8a armeabi-v7a
-APP_PLATFORM := android-24
+APP_ABI := arm64-v8a
+APP_PLATFORM := android-21
 APP_STL := c++_static
-APP_CPPFLAGS := -std=c++17 -frtti -fexceptions
+APP_CPPFLAGS := -std=c++17
