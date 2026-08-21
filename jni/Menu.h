49c624910e7683ea1ad5733f09691cf2878168af
Tvman <elijahpauley702@gmail.com>
2026-08-20 21:22:19 -0400

    Update Menu.h

--- jni/Menu.h
+++ jni/Menu.h
@@ -12,17 +12,13 @@ class TvMenuQuest {
 public:
     static bool isOpen;
     static int currentCategory;
-    static int currentMod;
 
-    // Settings
     static bool roundedCorners;
     static float menuAlpha;
-    static bool showBackground;
 
     static const std::vector<Category> categories;
     static std::map<std::string, bool> modStates;
 
     static void Init();
     static void Draw();
-    static void Toggle();
 };
