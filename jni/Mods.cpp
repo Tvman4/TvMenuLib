0ead7fb8d3cb1de7d111c50f79c865360d4850d1
Tvman <elijahpauley702@gmail.com>
2026-08-20 21:23:46 -0400

    Update Mods.cpp

--- jni/Mods.cpp
+++ jni/Mods.cpp
@@ -19,9 +19,6 @@ static std::atomic<bool> tagAllOn{false};
 static std::atomic<bool> orbitOn{false};
 static std::atomic<bool> ghostOn{false};
 static std::atomic<bool> invisOn{false};
-static std::atomic<bool> fullbrightOn{false};
-static std::atomic<bool> espOn{false};
-static std::atomic<bool> tracersOn{false};
 
 static void* ResolveICall(const char* name) {
     void* handle = dlopen("libil2cpp.so", RTLD_LAZY);
@@ -34,17 +31,13 @@ static void* ResolveICall(const char* name) {
 void Mods::SetLongArms(bool enabled) {
     longArmsOn = enabled;
     LOGI("Long Arms %s", enabled ? "ON" : "OFF");
-    void* setScale = ResolveICall("UnityEngine.Transform::set_localScale(UnityEngine.Vector3)");
-    if (setScale) LOGI("Long Arms scale ready");
 }
 
 void Mods::SetSpeed(bool enabled) {
     speedOn = enabled;
-    LOGI("Speed Boost %s", enabled ? "ON" : "OFF");
+    LOGI("Speed %s", enabled ? "ON" : "OFF");
     if (enabled) {
-        std::thread([] {
-            while (speedOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
+        std::thread([] { while (speedOn) std::this_thread::sleep_for(std::chrono::milliseconds(16)); }).detach();
     }
 }
 
@@ -52,42 +45,23 @@ void Mods::SetFly(bool enabled) {
     flyOn = enabled;
     LOGI("Fly %s", enabled ? "ON" : "OFF");
     if (enabled) {
-        std::thread([] {
-            while (flyOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
+        std::thread([] { while (flyOn) std::this_thread::sleep_for(std::chrono::milliseconds(16)); }).detach();
     }
 }
 
 void Mods::SetNoclip(bool enabled) {
     noclipOn = enabled;
     LOGI("Noclip %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (noclipOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
 }
 
 void Mods::SetGravity(bool enabled) {
     gravityOn = enabled;
-    LOGI("Low Gravity %s", enabled ? "ON" : "OFF");
-    void* grav = ResolveICall("UnityEngine.Physics::set_gravity(UnityEngine.Vector3)");
-    if (grav) LOGI("Gravity method ready");
-    if (enabled) {
-        std::thread([] {
-            while (gravityOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
+    LOGI("Gravity %s", enabled ? "ON" : "OFF");
 }
 
 void Mods::SetSuperJump(bool enabled) {
     superJumpOn = enabled;
     LOGI("Super Jump %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (superJumpOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
 }
 
 void Mods::SetZeroGravity(bool enabled) {
@@ -97,84 +71,50 @@ void Mods::SetZeroGravity(bool enabled) {
 void Mods::SetAirJump(bool enabled) {
     airJumpOn = enabled;
     LOGI("Air Jump %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (airJumpOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
 }
 
 void Mods::TagAll(bool enabled) {
     tagAllOn = enabled;
     LOGI("Tag All %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (tagAllOn) std::this_thread::sleep_for(std::chrono::milliseconds(180));
-        }).detach();
-    }
 }
 
 void Mods::Orbit(bool enabled) {
     orbitOn = enabled;
     LOGI("Orbit %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (orbitOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
 }
 
 void Mods::GhostMonkey(bool enabled) {
     ghostOn = enabled;
     LOGI("Ghost Monkey %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (ghostOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
 }
 
 void Mods::InvisAll(bool enabled) {
     invisOn = enabled;
     LOGI("Invis All %s", enabled ? "ON" : "OFF");
-    if (enabled) {
-        std::thread([] {
-            while (invisOn) std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }).detach();
-    }
 }
 
 void Mods::UnlockCosmetics(bool enabled) {
-    LOGI("Unlock All Cosmetics %s (Client-sided)", enabled ? "ON" : "OFF");
+    LOGI("Unlock Cosmetics %s (Client)", enabled ? "ON" : "OFF");
 }
 
 void Mods::RainbowMonkey(bool enabled) {
-    LOGI("Rainbow Monkey %s (Client-sided)", enabled ? "ON" : "OFF");
+    LOGI("Rainbow Monkey %s (Client)", enabled ? "ON" : "OFF");
 }
 
 void Mods::Fullbright(bool enabled) {
-    fullbrightOn = enabled;
     LOGI("Fullbright %s", enabled ? "ON" : "OFF");
 }
 
 void Mods::ESP(bool enabled) {
-    espOn = enabled;
     LOGI("ESP %s", enabled ? "ON" : "OFF");
 }
 
 void Mods::Tracers(bool enabled) {
-    tracersOn = enabled;
     LOGI("Tracers %s", enabled ? "ON" : "OFF");
 }
 
 void Mods::Disconnect(bool enabled) {
-    LOGI("Disconnect from Lobby called");
-    void* disconnect = ResolveICall("PhotonNetwork::Disconnect()");
-    if (disconnect) {
-        LOGI("Photon Disconnect method found");
-    } else {
-        LOGI("Photon Disconnect not found - trying alternative");
-    }
+    LOGI("Disconnect called");
 }
 
 void Mods::ExecuteUniversalMod(const std::string& name, bool state) {
