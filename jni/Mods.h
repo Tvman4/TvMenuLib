771a2d03045fe2971e17f4e0b7d49166d32512e1
Tvman <elijahpauley702@gmail.com>
2026-08-20 20:18:43 -0400

    Update Mods.h

--- jni/Mods.h
+++ jni/Mods.h
@@ -12,10 +12,14 @@ public:
     static void SetGravity(bool enabled);
     static void SetSuperJump(bool enabled);
     static void SetZeroGravity(bool enabled);
+    static void SetAirJump(bool enabled);
     static void TagAll(bool enabled);
     static void Orbit(bool enabled);
-    static void Crasher(bool enabled);
+    static void GhostMonkey(bool enabled);
+    static void InvisAll(bool enabled);
     static void UnlockCosmetics(bool enabled);
-    static void GodMode(bool enabled);
+    static void RainbowMonkey(bool enabled);
     static void Fullbright(bool enabled);
+    static void ESP(bool enabled);
+    static void Tracers(bool enabled);
 };
