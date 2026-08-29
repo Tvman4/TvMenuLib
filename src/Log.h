0c1e148934cacab3d501feac919bbc485870aec7
Tvman <elijahpauley702@gmail.com>
2026-08-29 16:33:52 -0400

    Delete src directory

--- src/Log.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <android/log.h>
-#define TVMENU_TAG "TvMenu"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  TVMENU_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,  TVMENU_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TVMENU_TAG, __VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TVMENU_TAG, __VA_ARGS__)
--- src/Mods.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace Mods {
-void Register();
-void Tick();
-}
